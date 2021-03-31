00ae05a5842ada0e91f9f9536ac949607308a274
karlderkaefer <karlderkaefer@users.noreply.github.com>
2021-03-31 12:29:16 +0200

    install pip if not installed

--- src/scripts/install-pre-commit.sh
+++ src/scripts/install-pre-commit.sh
@@ -9,7 +9,10 @@ InstallPip() {
       echo "Need to install pip"
       curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
       sudo python3 /tmp/get-pip.py
+  else
+    echo "pip already installed"
   fi
+
 }
 
 Install() {
