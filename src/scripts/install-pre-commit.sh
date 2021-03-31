033f327c31d2dc08b0e61d157098331b501b8c71
karlderkaefer <karlderkaefer@users.noreply.github.com>
2021-03-31 12:23:05 +0200

    install pip if not installed

--- src/scripts/install-pre-commit.sh
+++ src/scripts/install-pre-commit.sh
@@ -2,13 +2,24 @@ BIN_PATH=${BIN_PATH:-"${HOME}/.local/bin"}
 TOOL_NAME="pre-commit"
 TOOL_VERSION=${TOOL_VERSION:-""}
 
+InstallPip() {
+  python3 -m pip list -q
+  exitCode=$?
+  if [ $exitCode -ne 0 ]; then
+      echo "Need to install pip"
+      curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
+      sudo python3 /tmp/get-pip.py
+  fi
+}
+
 Install() {
   if ! [ -x "$(command -v ${TOOL_NAME})" ]; then
     if [ -x "$(command -v python3)" ]; then
+      InstallPip
       if [ -z "${TOOL_VERSION}" ]; then
-        python3 -m pip3 install ${TOOL_NAME}
+        python3 -m pip install ${TOOL_NAME}
       else
-        python3 -m pip3 install "${TOOL_NAME}==${TOOL_VERSION}"
+        python3 -m pip install "${TOOL_NAME}==${TOOL_VERSION}"
       fi
     else
       echo "python3 not available"
