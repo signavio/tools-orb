ff6fa2229c28700881c7225da20a02b3bbc8ddaa
karlderkaefer <karlderkaefer@users.noreply.github.com>
2022-07-21 11:29:00 +0200

    add sops

--- src/scripts/install-sops.sh
+++ src/scripts/install-sops.sh
@@ -5,7 +5,7 @@ Install() {
   if ! [ -x "$(command -v ${TOOL_NAME})" ]; then
     mkdir -p "${BIN_PATH}"
     if [ "${TOOL_VERSION}" = "latest" ]; then
-      TOOL_VERSION=$(curl -s https://api.github.com/repos/mozilla/sops/releases/latest | grep -Po '"tag_name": "\K.*?(?=")')
+      TOOL_VERSION="$(curl -s https://api.github.com/repos/mozilla/sops/releases/latest | grep -Po '"tag_name": "\K.*?(?=")')"
     fi
     SOPS_BINARY="sops-v${TOOL_VERSION}.linux.amd64"
     curl -sL "https://github.com/cli/cli/releases/download/v${TOOL_VERSION}/${SOPS_BINARY}" -o "${BIN_PATH}/sops"
