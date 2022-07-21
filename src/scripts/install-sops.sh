fe213c5ffe4a7fc659c9cc29a1dba3c23d0e76ad
karlderkaefer <karlderkaefer@users.noreply.github.com>
2022-07-21 11:23:17 +0200

    [semver:minor] add sops binary

---
+++ src/scripts/install-sops.sh
@@ -0,0 +1,22 @@
+BIN_PATH=${BIN_PATH:-"${HOME}/.local/bin"}
+TOOL_NAME="sops"
+
+Install() {
+  if ! [ -x "$(command -v ${TOOL_NAME})" ]; then
+    mkdir -p "${BIN_PATH}"
+    if ["${TOOL_VERSION}" = "latest" ]; then
+      TOOL_VERSION=$(curl -s https://api.github.com/repos/mozilla/sops/releases/latest | grep -Po '"tag_name": "\K.*?(?=")')
+    fi
+    SOPS_BINARY="sops-v${TOOL_VERSION}.linux.amd64"
+    curl -sL "https://github.com/cli/cli/releases/download/v${TOOL_VERSION}/${SOPS_BINARY}" -o "${BIN_PATH}/sops"
+    chmod +x "${BIN_PATH}/sops"
+    "${BIN_PATH}/${TOOL_NAME}" --version
+  else
+    echo "${TOOL_NAME} already installed"
+  fi
+}
+
+ORB_TEST_ENV="bats-core"
+if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
+    Install
+fi
